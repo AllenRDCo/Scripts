1ef5e964a69f6d1e13c5adc1bfe647612409152b
AllenRDCo <jp.paul.allen@gmail.com>
2014-10-07 17:16:07 -0600

    first commit

---
+++ rm_zone_new.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+# Use to remove specific domains from BIND conf file
+# Pass in one domain at a time to the script
+
+use strict;
+use warnings;
+use Getopt::Std;
+
+my %args=();
+getopts('hf:d:l:', \%args);
+
+
+sub usage() {
+    print STDERR << "EOF";
+
+This script is used to remove one or more zone entries from the BIND config files.
+
+Usage: $0 [-f file] [-d domain] [-l domain-list]
+
+    -f    : BIND zone configuration file location
+    -d    : Single domain to remove zone for
+    -l    : File containing newline seperated list of domains to remove zones for
+    -h    : Help (this message)
+
+example: $0 -f /etc/bind/conf/external.conf -d domain.com
+
+EOF
+    exit;
+}
+
+
+
+if (defined $args{h}) {
+    &usage;
+}
+if (! defined $args{f}) {
+    print STDERR "No conf file specified\n";
+    &usage;
+}
+elsif (! defined $args{d} && ! defined $args{l}) {
+    print STDERR "No domain or list specified\n";
+    &usage;
+}
+elsif (defined $args{d} && defined $args{l}) {
+    print STDERR "Must only specify -d <domain> OR -l <domain_list>";
+    &usage;
+}
+
+unless ( -e $args{f} ) {
+    print STDERR "No file found\n";
+    exit 1;
+}
+if (defined $args{l}) {
+    unless ( -e $args{l} ) {
+        print STDERR "No file found\n";
+        exit 1;
+    }
+}
+
+
+open INFILE, "<$args{f}";
+open OUTFILE, ">$args{f}.mod";
+while(<INFILE>) {
+    if(/zone "$args{d}"/) {
+            print "Removing $args{d}\n";
+        while(<INFILE>) {
+            if(/^};/) {
+                print "Done with zone removal\n";
+                last;
+            }
+        }
+    } else {
+        print OUTFILE;
+    }
+}
+close INFILE;
+close OUTFILE;
+rename("$args{f}.mod", "$args{f}");
